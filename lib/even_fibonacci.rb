a51a9620631d53b2df3226e095d87bd831d844c8
axton21 <axton_n@yahoo.com>
2015-03-18 12:58:47 -0400

    this is lab

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,14 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(num)
+	fibonacci_array(num).inject(0){|sum, x| sum += x.even? ? x : 0 }
+end
+
+
+def fibonacci_array(num)
+	array = [1, 2]
+	x= 0
+	while array[x]+array[x+1] < num do
+		array << array[x]+array[x+1]
+		x += 1
+	end
+array
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,21 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+	attr_reader :num
+
+	def initialize(num)
+		@num = num
+	end
+
+	def sum
+		fibonacci_array(num).inject(0){|sum, x| sum += x.even? ? x : 0 }
+	end
+
+	def fibonacci_array(num)
+		array = [1, 2]
+		x= 0
+		while array[x]+array[x+1] < num do
+			array << array[x]+array[x+1]
+			x += 1
+		end
+		array
+	end
+end
